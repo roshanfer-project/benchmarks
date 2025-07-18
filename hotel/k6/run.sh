f2454ea3ae0c1dded36cd68f6d2e91a1c8308ced
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2025-07-18 10:44:55 +0000

    add ramping rate for rajomon (grpc)

--- hotel/k6/run.sh
+++ hotel/k6/run.sh
@@ -6,11 +6,18 @@ export K6_OTEL_EXPORT_INTERVAL="100ms"
 export K6_OTEL_GRPC_EXPORTER_ENDPOINT="192.168.1.100:4317"
 export K6_OTEL_GRPC_EXPORTER_INSECURE=true
 
+# if the first argument is grpc, set the script to ov-grpc.js
+if [ "$1" == "grpc" ]; then
+  script="ov-grpc.js"
+else
+  script="script.js"
+fi
+
 start_time=$(date +%s.%6N)
-k6 run script.js -o experimental-opentelemetry
+k6 run $script -o experimental-opentelemetry
 end_time=$(date +%s.%6N)
 
 echo "Write timestamps to a file"
-file="../../../experiments/ov-rate-vs-time/timestamps.csv"
+file="../../../experiments/ov-vs-time/timestamps.csv"
 echo "$start_time" > "$file"
 echo "$end_time" >> "$file"
