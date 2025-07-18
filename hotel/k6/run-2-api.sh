99f9cd5dd32e2ce8f78904fd2f8843b8b261635b
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2025-07-18 10:45:38 +0000

    add k6 scripts for 2 concurrent APIs

---
+++ hotel/k6/run-2-api.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export K6_OTEL_METRIC_PREFIX="k6_"
+export K6_OTEL_FLUSH_INTERVAL="30ms"
+export K6_OTEL_EXPORT_INTERVAL="30ms"
+export K6_OTEL_GRPC_EXPORTER_ENDPOINT="192.168.1.100:4317"
+export K6_OTEL_GRPC_EXPORTER_INSECURE=true
+
+# if the first argument is grpc, set the script to ov-grpc.js
+if [ "$1" == "grpc" ]; then
+  script="ov-grpc-2-api.js"
+else
+  script="script.js"
+fi
+
+start_time=$(date +%s.%6N)
+k6 run $script -o experimental-opentelemetry
+end_time=$(date +%s.%6N)
+
+echo "Write timestamps to a file"
+file="../../../experiments/ov-vs-time-2-api/timestamps.csv"
+echo "$start_time" > "$file"
+echo "$end_time" >> "$file"
