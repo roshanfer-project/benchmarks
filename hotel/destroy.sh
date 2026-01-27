8cf6bd8947d05e75b3af99105af2d9fc53bc048a
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2026-01-27 17:08:29 +0000

    update hotel k8s

--- hotel/destroy.sh
+++ hotel/destroy.sh
@@ -8,13 +8,15 @@ kubectl delete pod frontend search profile geo rate reservation user ingress \
     mongodb-geo mongodb-profile mongodb-rate mongodb-reservation mongodb-user \
     memcached-rate memcached-profile memcached-reserve --ignore-not-found
 
+kubectl delete deployment prometheus prometheus-pushgateway --ignore-not-found
+
 # Delete Services
 kubectl delete service hotel-frontend hotel-search hotel-profile hotel-geo hotel-rate hotel-reservation hotel-user ingress \
     frontend-grpc \
     mongodb-geo mongodb-profile mongodb-rate mongodb-reservation mongodb-user \
-    memcached-rate memcached-profile memcached-reserve --ignore-not-found
+    memcached-rate memcached-profile memcached-reserve prometheus prometheus-pushgateway prometheus-external --ignore-not-found
 
 # Delete ConfigMaps
-kubectl delete configmap hotel-config sidecar-configs --ignore-not-found
+kubectl delete configmap hotel-config sidecar-configs prometheus-config --ignore-not-found
 
 echo "Cleanup complete."
