04dadd8ea8985972857427540c97676d8b87bb0e
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2026-03-25 12:02:25 +0000

    update benchmarks

--- tests/fan-out-weighted/services/backend2/main.go
+++ tests/fan-out-weighted/services/backend2/main.go
@@ -56,7 +56,7 @@ func (s *Server) Run() error {
 
 
 func (s *Server) F3(ctx context.Context, req *pb.Request) (*pb.Response, error) {
-	utils.BusyLoop(320)
+	utils.BusyLoop(480)
 	md, _ := metadata.FromIncomingContext(ctx)
 	api := ""
 	if v := md.Get("api"); len(v) == 1 {
