8fe7eca5959652e65f832550bc085f8a1d08e596
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2026-04-04 15:23:15 +0000

    add fanout-3

---
+++ tests/fan-out-3/services/backend3/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"fanout3/pkg"
+	pb "fanout3/protobuf"
+	"fanout3/utils"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
+)
+
+
+type Server struct {
+	pb.UnimplementedBackend3Server
+}
+
+const serviceName = "backend3"
+var log = utils.GetLogger(serviceName)
+
+func (s *Server) Run() error {
+	log.Info("Initializing gRPC server...")
+	opts := pkg.GetServerOptions()
+	sidecar := utils.GetEnvVar("sidecar", false) == "true"
+	queuingExport := utils.GetEnvVar("queuing_export", false) == "true"
+	if sidecar {
+		if queuingExport {
+			opts = append(opts, grpc.ChainUnaryInterceptor(
+				utils.ContextPropagationInterceptor(),
+				utils.NewCounterState(serviceName).GetInterceptor()))
+		} else {
+			opts = append(opts, grpc.UnaryInterceptor(utils.ContextPropagationInterceptor()))
+		}
+	} else {
+		opts = append(opts, grpc.ChainUnaryInterceptor(
+			utils.ContextPropagationInterceptor(),
+			utils.NewCounterState(serviceName).GetInterceptor()))
+	}
+	srv := grpc.NewServer(opts...)
+	pb.RegisterBackend3Server(srv, s)
+
+	reflection.Register(srv)
+	port := 2000
+	if sidecar {
+		port = utils.StrToInt(utils.GetEnvVar("backend3_PORT", true))
+	}
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
+	}
+	return srv.Serve(lis)
+}
+
+
+func (s *Server) F4(ctx context.Context, req *pb.Request) (*pb.Response, error) {
+	utils.BusyLoop(288)
+
+	md, _ := metadata.FromIncomingContext(ctx)
+	api := ""
+	if v := md.Get("api"); len(v) == 1 {
+		api = v[0]
+	}
+	switch api {
+	case "f1":
+
+	default:
+	}
+	return &pb.Response{}, nil
+}
+
+
+func main() {
+	s := &Server{}
+	log.Info("Starting server...")
+	if err := s.Run(); err != nil {
+		log.Error("Server failed", "error", err)
+	}
+}
