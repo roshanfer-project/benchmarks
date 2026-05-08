b3df502c2812547e7abce031581040d619d08cac
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2026-05-08 00:02:48 +0000

    add ingress stress benchmark

---
+++ tests/ingress-stress/utils/ennvars.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+func GetEnvVar(key string, required bool) string {
+	value := os.Getenv(key)
+	if value == "" && required {
+		log.Fatalf("Environment variable %s is required", key)
+	}
+	return value
+}
+
+func StrToInt(s string) int {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		log.Fatalf("Failed to convert string to int: %s", err)
+	}
+	return int(i)
+}
+
+func StrToFloat64(s string) float64 {
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		log.Fatalf("Failed to convert string to float64: %s", err)
+	}
+	return f
+}
+
+func ParseFloatString(value string) float64 {
+	f, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
